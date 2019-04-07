f03327ae247e524b1e7c4a3602fde28036842f23
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 19:32:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(the_array)
   if the_array.length == 1
-  the_string = the_array * ""
-  eslif 
+   the_string = the_array * ""
+   elsif the_array.length == 2
+   the_string = the_array
