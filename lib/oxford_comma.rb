53a8c73fc9467280cc15c418ee013d0c6dfb1f83
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 19:35:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(the_array)
   if the_array.length == 1
    the_string = the_array * ""
    elsif the_array.length == 2
-   the_string = the_array * "and"
+   the_string = the_array * " and "
      else
     str = "#{the_array[0...-1]*", "}, and #{the_array.last}"
   end
