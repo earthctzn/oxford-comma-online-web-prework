f719b11a711bc2f5f5336ce71fe5b6257d99a55e
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 19:32:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,5 @@ def oxford_comma(the_array)
   if the_array.length == 1
    the_string = the_array * ""
    elsif the_array.length == 2
-   the_string = the_array
+   the_string = the_array * "and"
+   
